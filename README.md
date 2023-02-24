dba16b8115216294b84740cdc449e4c5a4e18298
hideckies <hideckies@gmail.com>
2023-02-24 19:20:13 +0900

    edited README.md

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# Alternative RockYou
+# Alternative Rockyou
 
-This repository contains various encoded **rockyou.txt**.  
+This repository contains various encoded **rockyou.txt** such as **base64-rockyou.txt**, **md5-rockyou.txt**, and so on.  
 
-To use them, you need to extract as follow.
+To use them immediately, download from **[release page](https://github.com/hideckies/alternative-rockyou/releases)** and extract it as follow.
 
 ```sh
 gunzip base64-rockyou.txt.gz
 ```
 
-## Generate from Script
+## Generate Rockyou from Script
 
 If you want to generate some encoded **rockyou.txt** from Python script, run the following command.  
 Assume your system locates **rockyou.txt** under **`/usr/share/wordlists/`**.
@@ -18,4 +18,10 @@ Assume your system locates **rockyou.txt** under **`/usr/share/wordlists/`**.
 python3 generator.py base32 > base32-rockyou.txt
 # Base64
 python3 generator.py base64 > base64-rockyou.txt
+# MD5
+python3 generator.py md5 > md5-rockyou.txt
+# SHA256
+python3 generator.py sha256 > sha256-rockyou.txt
+# SHA512
+python3 generator.py sha512 > sha512-rockyou.txt
 ```
--- generator.py
+++ generator.py
@@ -1,6 +1,8 @@
 import base64
 import codecs
+import hashlib
 import sys
+
 
 def main():
     etype = sys.argv[1]
@@ -9,16 +11,21 @@ def main():
         words = [w.rstrip() for w in f.readlines()]
     
     for word in words:
-        if etype == "base16":
-            encoded = base64.b16encode(codecs.encode(word, 'ascii', errors='ignore'))
-        elif etype == "base32":
+        if etype == "base32":
             encoded = base64.b32encode(codecs.encode(word, 'ascii', errors='ignore'))
+            print(encoded)
         elif etype == "base64":
             encoded = base64.b64encode(codecs.encode(word, 'ascii', errors='ignore'))
-        elif etype == "base85":
-            encoded = base64.b85encode(codecs.encode(word, 'ascii', errors='ignore'))
-
-        print(encoded.decode('utf-8'))
+            print(encoded)
+        elif etype == "md5":
+            encoded = hashlib.md5(codecs.encode(word, 'ascii', errors='ignore'))
+            print(encoded.hexdigest())
+        elif etype == "sha256":
+            encoded = hashlib.sha256(codecs.encode(word, 'ascii', errors='ignore'))
+            print(encoded.hexdigest())
+        elif etype == "sha512":
+            encoded = hashlib.sha512(codecs.encode(word, 'ascii', errors='ignore'))
+            print(encoded.hexdigest())
 
 
 if __name__ == '__main__':
